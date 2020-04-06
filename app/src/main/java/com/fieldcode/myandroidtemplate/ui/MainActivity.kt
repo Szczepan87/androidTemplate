c8655f8c63a1508124b1514c61e5142d6d5560d9
Szczepan <l.szczepan@wp.pl>
2020-04-06 12:32:00 +0200

    toolbar set for nav controller

--- app/src/main/java/com/fieldcode/myandroidtemplate/ui/MainActivity.kt
+++ app/src/main/java/com/fieldcode/myandroidtemplate/ui/MainActivity.kt
@@ -3,7 +3,12 @@ package com.fieldcode.myandroidtemplate.ui
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.fieldcode.myandroidtemplate.R
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +18,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
     }
 }
